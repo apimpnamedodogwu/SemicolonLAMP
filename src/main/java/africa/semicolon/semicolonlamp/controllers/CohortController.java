713ea7e8cab89c55a9e3514ddbfd12d795516475
apimpnamedodogwu <edenelenwoke@gmail.com>
2022-07-26 23:18:18 -0700

    LAMP Implementation

---
+++ src/main/java/africa/semicolon/semicolonlamp/controllers/CohortController.java
@@ -0,0 +1,104 @@
+package africa.semicolon.semicolonlamp.controllers;
+
+
+import africa.semicolon.semicolonlamp.request.CohortCreateRequest;
+import africa.semicolon.semicolonlamp.services.CohortService;
+import africa.semicolon.semicolonlamp.services.lampExceptions.CohortException;
+import africa.semicolon.semicolonlamp.services.lampExceptions.EmptyFieldException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("api/v1/cohort")
+@Slf4j
+public class CohortController {
+    @Autowired
+    CohortService cohortService;
+
+    @PatchMapping("edit/name/{cohortId}")
+    public ResponseEntity<?> changeCohortName(@PathVariable String cohortId, @RequestParam String newName) {
+        try {
+            cohortService.changeCohortName(cohortId, newName);
+            var mess = "Your cohort's name has been changed to " + newName + " successfully!";
+            return new ResponseEntity<>(mess, HttpStatus.CREATED);
+        } catch (CohortException message) {
+            var display = message.getMessage();
+            return new ResponseEntity<>(display, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("update/status/{cohortId}")
+    public ResponseEntity<?> updateCohortStatus(@PathVariable String cohortId, @RequestParam String newStatus) {
+        try {
+            cohortService.updateCohortStatus(cohortId, newStatus);
+            var mess = "Your cohort's status has been updated to " + newStatus + " successfully!";
+            return new ResponseEntity<>(mess, HttpStatus.CREATED);
+        } catch (CohortException message) {
+            var display = message.getMessage();
+            return new ResponseEntity<>(display, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("inSession")
+    public ResponseEntity<?> getAllInSession() {
+        var session = cohortService.getAllInSessionCohorts();
+        return new ResponseEntity<>(session, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{cohortId}")
+    public ResponseEntity<?> deleteCohort(@PathVariable String cohortId) {
+        try {
+            cohortService.deleteCohort(cohortId);
+            var display = "Cohort with id number " + cohortId + " has been deleted successfully!";
+            return new ResponseEntity<>(display, HttpStatus.OK);
+        } catch (CohortException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("completed")
+    public ResponseEntity<?> getAllCompleted() {
+        var completed = cohortService.getAllCompletedCohort();
+        return new ResponseEntity<>(completed, HttpStatus.OK);
+    }
+
+    @GetMapping("pending")
+    public ResponseEntity<?> getAllPending() {
+        var pending = cohortService.getAllPendingCohort();
+        return new ResponseEntity<>(pending, HttpStatus.OK);
+    }
+
+    @PostMapping("createCohort/request")
+    public ResponseEntity<?> createCohort(@RequestBody CohortCreateRequest request) {
+        try {
+            cohortService.createCohort(request);
+            var display = "Your cohort has been created successfully!";
+            return new ResponseEntity<>(display, HttpStatus.CREATED);
+        } catch (EmptyFieldException | CohortException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("{cohortId}")
+    public ResponseEntity<?> getCohort(@PathVariable String cohortId) {
+        try {
+            var cohort = cohortService.getCohort(cohortId);
+            return new ResponseEntity<>(cohort, HttpStatus.OK);
+        } catch (CohortException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("allCohorts")
+    public ResponseEntity<?> getAllCohorts() {
+        var allCohorts = cohortService.getAllCohorts();
+        return new ResponseEntity<>(allCohorts, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/africa/semicolon/semicolonlamp/controllers/UserController.java
@@ -0,0 +1,128 @@
+package africa.semicolon.semicolonlamp.controllers;
+
+import africa.semicolon.semicolonlamp.request.UserRegistrationRequest;
+import africa.semicolon.semicolonlamp.request.UserUpdateRequest;
+import africa.semicolon.semicolonlamp.services.UserService;
+import africa.semicolon.semicolonlamp.services.lampExceptions.CohortException;
+import africa.semicolon.semicolonlamp.services.lampExceptions.EmptyFieldException;
+import africa.semicolon.semicolonlamp.services.lampExceptions.ExistingEmailException;
+import africa.semicolon.semicolonlamp.services.lampExceptions.InvalidUserIdException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/user")
+@Slf4j
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PatchMapping("/change/{userId}/newType")
+    public ResponseEntity<?> changeUserType(@PathVariable String userId, @RequestParam String newType) {
+        try {
+            userService.changeUserType(userId, newType);
+            var display = "User with id number " + userId + " has been changed to " + newType + " successfully!";
+            return new ResponseEntity<>(display, HttpStatus.OK);
+        } catch (InvalidUserIdException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("{userId}/request")
+    public ResponseEntity<?> updateUserDetails(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        try {
+            userService.updateUserDetails(userId, request);
+            var display = "User with id number " + userId + " has been updated successfully!";
+            return new ResponseEntity<>(display, HttpStatus.OK);
+        } catch (ExistingEmailException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("allElders")
+    public ResponseEntity<?> getAllElders() {
+        var elders = userService.getAllElders();
+        return new ResponseEntity<>(elders, HttpStatus.OK);
+    }
+
+    @GetMapping("allAncestors")
+    public ResponseEntity<?> getAllAncestors() {
+        var ancestors = userService.getAllAncestors();
+        return new ResponseEntity<>(ancestors, HttpStatus.OK);
+    }
+
+    @GetMapping("allNatives")
+    public ResponseEntity<?> getAllNatives() {
+        var natives = userService.getAllNatives();
+        return new ResponseEntity<>(natives, HttpStatus.OK);
+    }
+
+    @GetMapping("{cohortId}")
+    public ResponseEntity<?> getAllNativesInACohort(@PathVariable String cohortId) {
+        try {
+            var nativesInACohort = userService.getAllNativesInACohort(cohortId);
+            return new ResponseEntity<>(nativesInACohort, HttpStatus.OK);
+        } catch (CohortException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<?> getUser(@PathVariable String userId) {
+        try {
+            var user = userService.getUser(userId);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (InvalidUserIdException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("alLUsers")
+    public ResponseEntity<?> getAllUsers() {
+        var users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("createANative/request")
+    public ResponseEntity<?> createANative(@RequestBody UserRegistrationRequest request) {
+        try {
+            userService.createUserNative(request);
+            var display = "Congratulations, you are now a native!";
+            return new ResponseEntity<>(display, HttpStatus.CREATED);
+        } catch (ExistingEmailException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("createAnAncestor/request")
+    public ResponseEntity<?> createAnAncestor(@RequestBody UserRegistrationRequest request) {
+        try {
+            userService.createUserAncestor(request);
+            var display = "Congratulations, you made it through hell and back. You are now an Ancestor!";
+            return new ResponseEntity<>(display, HttpStatus.CREATED);
+        } catch (ExistingEmailException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("createAnElder/request")
+    public ResponseEntity<?> createAnElder(@RequestBody UserRegistrationRequest request) {
+        try {
+            userService.createUserElder(request);
+            var display = "Welcome back to another form of hell loop!";
+            return new ResponseEntity<>(display, HttpStatus.CREATED);
+        } catch (ExistingEmailException message) {
+            var displayMessage = message.getMessage();
+            return new ResponseEntity<>(displayMessage, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
