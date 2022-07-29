d549ee6345ff6c571c32d33c150b7fa49f3b130f
apimpnamedodogwu <edenelenwoke@gmail.com>
2022-07-29 12:45:10 -0700

    LAMP Mock Test

--- src/test/java/africa/semicolon/semicolonlamp/services/UserServiceTest.java
+++ src/test/java/africa/semicolon/semicolonlamp/services/UserServiceTest.java
@@ -1,31 +1,29 @@
 package africa.semicolon.semicolonlamp.services;
 
+import africa.semicolon.semicolonlamp.models.User;
 import africa.semicolon.semicolonlamp.repositories.UserRepository;
+import africa.semicolon.semicolonlamp.request.UserRegistrationRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
     @Mock
     UserRepository userRepository;
-//    @Autowired
+    @InjectMocks
     UserServiceImplementation userServiceImplementation;
 
 
-    @BeforeEach
-    void setUp() {
-        userServiceImplementation = new UserServiceImplementation();
-    }
+
 
     @Test
     void changeUserType() {
@@ -67,6 +65,18 @@ class UserServiceTest {
 
     @Test
     void createUserNative() {
+        UserRegistrationRequest request = new UserRegistrationRequest();
+//        User user = new User();
+        request.setFirstName("Eden");
+        request.setLastName("Elenwoke");
+        request.setEmail("eden.kwinesta@gmail.com");
+        request.setPassword("12345678");
+        userServiceImplementation.createUserNative(request);
+        ArgumentCaptor<User> UserArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(UserArgumentCaptor.capture());
+        var capturedUser = UserArgumentCaptor.getValue();
+        assertThat(capturedUser).isEqualTo(request);
+
     }
 
     @Test
